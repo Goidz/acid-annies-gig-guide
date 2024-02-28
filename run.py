ca3e78edbd7308ce8274e89d680448de36e2de7d
Gideon Horn <gideon.h99@gmail.com>
2024-02-28 11:25:31 +0000

    Innitial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from datetime import datetime
+
+the_date = datetime.now().date()
+print(the_date)
+
+genre = ""
+genres = {"black metal", "blues", "death metal", "stoner", "rock", "doom", "thrash metal", 
+"prog", "heavy metal", "power metal", "jazz", "speed_metal", "core"}
+print(f"These are your genres to pick from: {genres}.")
+
+"""
+Selecting a genre from the genres list.
+"""
+def select_genre():
+    choice = input("Please choose a genre: \n")
+    while choice == genres:
+        print(select_genre)
+
+        
+        
+
+
+select_genre()    
