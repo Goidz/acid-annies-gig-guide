89cff9686c0794afdeb8fa2c705d071e4bed8fcb
Gideon Horn <gideon.h99@gmail.com>
2024-02-28 13:28:48 +0000

    Add: Function to select music genre from list validates input

--- run.py
+++ run.py
@@ -3,18 +3,20 @@ from datetime import datetime
 the_date = datetime.now().date()
 print(the_date)
 
-genre = ""
-genres = {"black metal", "blues", "death metal", "stoner", "rock", "doom", "thrash metal", 
-"prog", "heavy metal", "power metal", "jazz", "speed_metal", "core"}
-print(f"These are your genres to pick from: {genres}.")
+
+genres = ["black metal", "blues", "death metal", "stoner", "rock", "doom", "thrash metal", 
+"prog", "heavy metal", "power metal", "jazz", "speed metal", "core\n"]
+print(f"These are your genres to pick from: {genres}.\n")
 
 """
 Selecting a genre from the genres list.
 """
 def select_genre():
-    choice = input("Please choose a genre: \n")
-    while choice == genres:
-        print(select_genre)
+    choice = input("Please choose a genre (in lower case): \n")
+    if choice in genres:
+        print(f"You chose {choice}.")
+    else:
+        print("Incorrect. Please select a genre from the list")
 
         
         
