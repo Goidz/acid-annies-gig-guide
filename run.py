cd22a828fc5291a0139ba1c37f5e82a84df8450d
Gideon Horn <gideon.h99@gmail.com>
2024-02-28 16:06:10 +0000

    Add: event title and location input. Basic layouts.

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 from datetime import datetime
 
-the_date = datetime.now().date()
-print(the_date)
+#the_date = datetime.now().date()
+#print(the_date)
 
+"""
+Event title and location input.
+"""
+def enter_event_title():
+    event_title = input("Enter artist(s)/event.(Seperate with commas) \n")
+    event_location = input("Enter location/venue \n")
+    print(f"Tonight {event_title} live at {event_location}")
 
+enter_event_title()
+
+"""
+List of genres for select_genre function.
+"""
 genres = ["black metal", "blues", "death metal", "stoner", "rock", "doom", "thrash metal", 
-"prog", "heavy metal", "power metal", "jazz", "speed metal", "core\n"]
+"prog", "heavy metal", "power metal", "jazz", "speed metal", "core"]
 print(f"These are your genres to pick from: {genres}.\n")
 
 """
@@ -16,10 +28,6 @@ def select_genre():
     if choice in genres:
         print(f"You chose {choice}.")
     else:
-        print("Incorrect. Please select a genre from the list")
-
-        
-        
-
+        print("Incorrect. Please select a genre from the list. In lowercase")
 
 select_genre()    
